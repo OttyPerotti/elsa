8eb57e68484ab9dd017233e605ebcfe96bc5002e
john de winton <john.dewinton@yahoo.co.uk>
2019-05-16 16:16:49 +0100

    completed validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :hostel
   belongs_to :room
-  validates:
-  validates
+  validates :date, presence: true
+  validates :nights, presence: true
+  validates :user_id, uniqueness: true, presence: true
+  validates :hostel_id, uniqueness: true, presence: true
+  validates :room_id, uniqueness: true, presence: true
 end
