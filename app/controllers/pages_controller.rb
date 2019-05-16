8bf668405ac382e2dabca79114bd0235565f8979
Royden Cooper <royden@scocoinvestments.com>
2019-05-16 18:06:42 +0100

    final commit for project day 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,22 @@
 class PagesController < ApplicationController
+
   def home
+  end
+
+# Royden's name
+  def show_partner_dashboard
+  end
+
+# Royden's name
+  def edit_payment_method
+  end
+
+# John's job
+  def update_payment_method
+  end
+
+# John's job
+  def show_partner_invoices
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # adding routes for the b2b dashboard below:
+  get 'show_partner_dashboard/:id', to: 'pages#show_partner_dashboard'
+  get 'edit_payment_method/:id/edit', to: 'pages#edit_payment_method'
+  patch 'update_payment_method/:id', to: 'pages#update_payment_method'
+  get 'show_partner_invoices/:id' to: 'pages#show_partner_invoices'
+
 end
