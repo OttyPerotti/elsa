131b4bc31a6f9516cc4833e4a8540e931bbbe532
Royden Cooper <royden@scocoinvestments.com>
2019-05-16 16:39:01 +0100

    fixed the migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_16_153019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "nights"
+    t.bigint "user_id"
+    t.bigint "hostel_id"
+    t.bigint "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hostel_id"], name: "index_bookings_on_hostel_id"
+    t.index ["room_id"], name: "index_bookings_on_room_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "hostels", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city_name"
+    t.bigint "user_id"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_hostels_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "beds"
+    t.string "type"
+    t.bigint "hostel_id"
+    t.boolean "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hostel_id"], name: "index_rooms_on_hostel_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "hostels"
+  add_foreign_key "bookings", "rooms"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "hostels", "users"
+  add_foreign_key "rooms", "hostels"
 end
