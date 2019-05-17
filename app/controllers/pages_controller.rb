6739dcd459da03d90c8b2f9cbf815bb50e521f46
Royden Cooper <royden@scocoinvestments.com>
2019-05-17 15:10:01 +0100

    resolving issues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,21 +7,5 @@ class PagesController < ApplicationController
     # @hostels = Hostel.all
   end
 
-# # Royden's name
-#   def show_partner_dashboard
-
-#   end
-
-# # Royden's name
-#   def edit_payment_method
-#   end
-
-# # John's job
-#   def update_payment_method
-#   end
-
-# # John's job
-#   def show_partner_invoices
-#   end
 
 end
