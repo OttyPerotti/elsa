53800c9bc4cda4f469fb40ac39b6e60e0384fe26
john de winton <john.dewinton@yahoo.co.uk>
2019-05-17 15:01:19 +0100

    resolve conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   before_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
