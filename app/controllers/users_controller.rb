e9e85f868b73dd6bd04fcb258b83f636d0086496
john de winton <john.dewinton@yahoo.co.uk>
2019-05-17 18:13:34 +0100

    done some of the table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
-    @hostels = Hostel.where(user: current_user)
-    authorize @user
+    @hostel = Hostel.where(user: current_user)
+    @latest_bookings = @hostel.first.bookings.order(created_at: :desc).limit(5)
   end
+
 end
