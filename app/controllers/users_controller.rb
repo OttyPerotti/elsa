2c543b805acf06aad756f5decbe33ed352ffae40
john de winton <john.dewinton@yahoo.co.uk>
2019-05-19 23:17:03 +0100

    created an authorization for User_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-
+before_action :check_authorization
   def show
     @user = User.find(params[:id])
     @hostel = Hostel.where(user: current_user)
     @latest_bookings = @hostel.first.bookings.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  private
+  def check_authorization
+    authorize :user
   end
 
 end
