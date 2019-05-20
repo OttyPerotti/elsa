0d01d828ec2c46b6bbc858e192e3264e37e0e81c
Royden Cooper <royden@scocoinvestments.com>
2019-05-20 11:20:58 +0100

    updating my branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   USER_TYPES = ["owner", "guest"]
   NATIONALITIES = ["American", "British", "Italian", "Polish"]
-  has_many :hostels # add an if condition for user_type
+  has_many :hostels # add an if statement depending on user_type
   has_many :bookings
 
   # validates :user_type, presence: true
