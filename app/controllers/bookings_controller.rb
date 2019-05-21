1892e28f60d4f79cd474a4d3c21e8290a003ee27
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-21 16:29:18 +0100

    not sure

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to booking_path(@booking), notice: "Booking successfully created"
     else
       render :new
     end
