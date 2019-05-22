26909cda71c2681fe809553e69b5650d70c13135
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-22 15:34:51 +0100

    minor changes to bookings css

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,7 @@ class Booking < ApplicationRecord
   # validates :date, presence: true // i remove this as it will require a gem
   # that handles time frame of the booking with start and end date
   # revisit with MVP 2
-  validates :nights, presence: true, numericality: { only_integer: true }
+  # validates :nights, numericality: { only_integer: true }
 
   # validates :room_id, uniqueness: true, presence: true // this is being commented
   # out as MPV 1 while not have rooms db as per TA's advice
